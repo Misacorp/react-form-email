e25c43943969bd0e815a22b57794ff5346c79728
Misacorp <misa.jokisalo@gmail.com>
2017-11-09 15:49:12 +0200

    Simplified server, tweaked settings for Heroku

--- server/app.js
+++ server/app.js
@@ -1,58 +1,27 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+import bodyParser from 'body-parser'
+import express from 'express'
+import path from 'path'
+const app = express();
+const mail = require('./routes/mail');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var mail = require('./routes/mail');
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
-var app = express();
+const router = express.Router()
 
-// Get reference to the client build directory
-var staticFiles = express.static(path.join(__dirname, '../../client/build'))
+const staticFiles = express.static(path.join(__dirname, '../../client/build'))
+app.use(staticFiles)
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(router)
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// pass the static files (react app) to the express app. 
-app.use(staticFiles);
-app.use(logger('dev'));
-app.use(bodyParser.json({
-  limit: '50mb'
-}));
-app.use(bodyParser.urlencoded({
-  extended: false,
-  limit: '50mb'
-}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
 app.use('/mail', mail);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// any routes not picked up by the server api will be handled by the react router
+app.use('/*', staticFiles)
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.set('port', (process.env.PORT || 3001))
+app.listen(app.get('port'), () => {
+  console.log(`Listening on ${app.get('port')}`)
+})
 
 module.exports = app;
